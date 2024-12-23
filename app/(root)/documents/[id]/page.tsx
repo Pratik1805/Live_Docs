206200cafd3c1a5caabd5b9fe1f65856ea50dac5
Pratik1805 <pratikpandey0532@gmail.com>
2024-12-23 22:27:38 +0530

    Added Collaborative Users to be displayed in the particular documents

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,12 +1,31 @@
 import React from 'react'
 import CollaborativeRoom from "@/components/CollaborativeRoom";
+import {currentUser} from "@clerk/nextjs/server";
+import {redirect} from "next/navigation";
+import {getDocument} from "@/lib/actions/room.actions";
 
 
 
-const Document = () => {
+const Document = async ({ params: {id} }: SearchParamProps) => {
+
+    const clerkUser = await currentUser();
+
+    if(!clerkUser) redirect('/sign-in');
+
+    const room = await getDocument({
+        roomId: id,
+        userId: clerkUser.emailAddresses[0].emailAddress,
+    })
+
+    if(!room) redirect('/');
+
+    //TODO: Assess the permissions of the user to access documents
     return (
         <main className={"w-full flex flex-col items-center"}>
-            <CollaborativeRoom/>
+            <CollaborativeRoom
+                roomId = {id}
+                roomMetaData = {room.metadata}
+            />
         </main>
     )
 }
